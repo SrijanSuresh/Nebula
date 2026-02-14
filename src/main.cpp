b4615d8869e9bcbf5a672a43f3c87b81dffd4718
Srijan Suresh <srijansuresh04@gmail.com>
2026-02-14 17:01:03 -0600

    triangle in openGL

--- src/main.cpp
+++ src/main.cpp
@@ -5,32 +5,68 @@ using namespace std;
 
 int main(){
 
+    // graphics lib framework init
     if (!glfwInit()){
         return -1;
     }
-    
+
+    // window setup
     GLFWwindow* window = glfwCreateWindow(800, 600, "WindowGL", NULL, NULL);
     
     if (window == NULL){
-      glfwTerminate();
-      return -1;
+      	glfwTerminate();
+      	return -1;
     }
     glfwMakeContextCurrent(window);
     
+    // loading glad to retrieve latest runtimes for glfw
     if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)){
-      return -1;
+      	return -1;
     }
-    
-    while(!glfwWindowShouldClose(window)){
-    // clear screen
-    glClearColor(0.05f, 0.05f, 0.1f, 1.0f);
-    glClear(GL_COLOR_BUFFER_BIT);
 
-    // swap and poll
-    glfwSwapBuffers(window);
-    glfwPollEvents();
+    // vertices for triangle
+    float vertices[] = {
+    	-0.5f, -0.5f, 0.0f, // bottom-left
+    	0.5f, -0.5f, 0.0f, // bottom-right
+	0.0f, 0.5f, 0.0f // up
     
+    }; 
+    // create a vector array object and buffer obj then bind it to GPU
+    GLuint vao, vbo;
+    glGenVertexArrays(1, &vao);
+    glGenBuffers(1, &vbo);
+
+    glBindVertexArray(vao);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    
+    // creating buffer data
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    
+    // read binary data for vbo
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+    glEnableVertexAttribArray(0);
+
+    // unbind after setup is done
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
+    // window loop
+    while(!glfwWindowShouldClose(window)){
+    	// clear screen
+    	glClearColor(0.05f, 0.05f, 0.1f, 1.0f);
+    	glClear(GL_COLOR_BUFFER_BIT);
+        
+	// draw
+	glBindVertexArray(vao);
+	glDrawArrays(GL_TRIANGLES, 0, 3);
+
+    	
+	// swap and poll
+    	glfwSwapBuffers(window);
+    	glfwPollEvents();
     }
+    // unbind vector array object
+    glBindVertexArray(0);
 
     glfwTerminate();
     return 0;
