e45502238676f2f2c7e06168eec4fed617bdc32d
Srijan Suresh <srijansuresh04@gmail.com>
2026-02-14 21:17:37 -0600

    added stars + pulsing animation with uniform time

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,21 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
+#include <ctime>
+#include <cstdlib>
+#include <vector>
 using namespace std;
 
 
 // importing shader source
-
+// 1. Vector Shader Soruce
 const char* vertexShaderSource = R"(
     #version 450 core
     layout (location = 0) in vec3 aPos;
+    uniform float u_time; // retrieve uniform time from cpu to shader
     void main() {
-        gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+        gl_Position = vec4(aPos, 1.0);
+	gl_PointSize = 2.0;
     }
 )";
 
@@ -18,10 +23,33 @@ const char* vertexShaderSource = R"(
 const char* fragmentShaderSource = R"(
     #version 450 core
     out vec4 FragColor;
+    uniform float u_time;
     void main() {
-        FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f); // Orange color
+	float pulse = (sin(u_time * 2.0) + 1.0) / 2.0; // oscillation between 0 and 1
+        FragColor = vec4(1.0, 0.8, 0.5, pulse); // yellow-orange color
     }
 )";
+
+vector<float>generateStars(int count){
+	/* we are not using 2d vector since lookup of 2d vector is pretty 
+	 inefficient for gpu, so we flatten them into 1d contiguos array.
+	 so we have like [x_1,y_1,z_1,....,x_n,y_n,z_n] from this we can
+	 iterate position by setting:
+	 x index -> i*3, y index -> i*3+1, z index-> i*3+2 */
+	
+	vector<float>stars;
+	stars.reserve(count*3); // x,y,z
+
+	for(int i = 0; i < count; i++){
+	    float x = (float)rand()/(float)RAND_MAX * 2.0f - 1.0f;
+	    float y = (float)rand()/(float)RAND_MAX * 2.0f - 1.0f;
+	    float z = (float)rand()/(float)RAND_MAX * 2.0f - 1.0f;
+	    stars.push_back(x);
+	    stars.push_back(y);
+	    stars.push_back(z);
+	}
+	return stars;
+}
 
 
 int main(){
@@ -45,13 +73,9 @@ int main(){
       	return -1;
     }
 
-    // vertices for triangle
-    float vertices[] = {
-    	-0.5f, -0.5f, 0.0f, // bottom-left
-    	0.5f, -0.5f, 0.0f, // bottom-right
-	0.0f, 0.5f, 0.0f // up
-    
-    }; 
+    // vertices for stars
+    vector<float> vertices = generateStars(1000);
+
     // create a vector array object and buffer obj then bind it to GPU
     GLuint vao, vbo;
     glGenVertexArrays(1, &vao);
@@ -61,7 +85,7 @@ int main(){
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
     
     // creating buffer data
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, vertices.size()*sizeof(float), vertices.data(), GL_STATIC_DRAW);
     
     // read binary data for vbo
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
@@ -84,7 +108,13 @@ int main(){
     glAttachShader(shaderProgram, vertexShader);
     glAttachShader(shaderProgram, fragmentShader);
     glLinkProgram(shaderProgram);
+
+    // get u_time back from gpu
+    int timeLoc = glGetUniformLocation(shaderProgram, "u_time");
     
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA); // to make star pulsing to look like fade
+    glEnable(GL_PROGRAM_POINT_SIZE); // WSL shader change fix
     // window loop
     while(!glfwWindowShouldClose(window)){
     	// clear screen
@@ -94,7 +124,9 @@ int main(){
 	// attach shaders then draw
 	glBindVertexArray(vao);
 	glUseProgram(shaderProgram);
-	glDrawArrays(GL_TRIANGLES, 0, 3);
+	float timeVal = glfwGetTime(); // geting uniform time for star pulsing
+	glUniform1f(timeLoc, timeVal);
+	glDrawArrays(GL_POINTS, 0, 1000);
 
     	
 	// swap and poll
